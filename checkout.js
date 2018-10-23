c4d22bc5f4e3704980d948db0884e0c7121f6f32
Jamie Kinkead <jkinkead@gmail.com>
2018-10-23 13:20:42 -0700

    more checkout improvements

--- checkout.js
+++ checkout.js
@@ -1,5 +1,5 @@
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
+  
+    const elems = document.querySelectorAll('select');
+    const instances = M.FormSelect.init(elems);
+ 
