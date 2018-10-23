f7f1d3ee2b7fda35f95f9af996f2ac8ec07da5fd
Nolan <nolan.hellyer@gmail.com>
2018-10-23 14:26:19 -0700

    added images and updated the product list

--- scripts/product-list.js
+++ scripts/product-list.js
@@ -1,10 +1,10 @@
-[
+module.exports = products = [
   {
   name : 'Vogner Char-King',
   price : 499.99,
   rating : 3.5,
   description : 'Turn the heat up with the Vogner Char King. With a full size grill and fine grain heat controls, the Vogner Char King takes your barbecuing to the next level.',
-  image : 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Gas_grill_20170807.jpg',
+  image : 'img/vogner-char-king.jpg',
   tags : ['grill', 'Char-King']
   },
   {
@@ -12,7 +12,7 @@
     price: 799.99,
     rating : 5,
     description : 'Our flagship grill, the Char-King Imperiale provides the best grilling experience you can achieve. Fully outfitted with Char-King\'s famous Accu-Heat™ Technology, Gas Stream™ burners and mult-layer grill, the Char-King Imperiale is the official State Grill of Texas.',
-    image : 'https://cdn.pixabay.com/photo/2015/06/29/16/41/bbq-825711_1280.jpg',
+    image : 'img/char-king-imperiale.jpg',
     tags : ['grill', 'Char-King']
   },
   {
@@ -20,15 +20,15 @@
     price : 399.99,
     rating : 4,
     description : 'The Vogner Citizen may be our entry level propane grill, but don\'t be fooled! The Vogner Citizen still cooks your meat thoroughly and cleanly, leaving you with the taste of your meat, not the heat you cooked it with.',
-    image : 'https://cdn.pixabay.com/photo/2015/02/26/13/55/barbeque-650352_1280.jpg',
+    image : 'img/vogner-citizen.jpg',
     tags : ['grill']
   },
   {
     name : 'Propane Tank',
     price : 24.99,
-    rating 4.5,
+    rating : 4.5,
     description : 'Buy a full 20 lb. tank of clean burning propane',
-    image : 'https://upload.wikimedia.org/wikipedia/commons/f/fe/Propane_tank_20lb.jpg',
+    image : 'img/new-tank.jpg',
     tags : ['propane', '20 lb']
   }
 ]
