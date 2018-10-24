c3973adeafd5148784d5aed852c5889751468310
Kevin Parine <kparine314@gmail.com>
2018-10-23 22:05:59 -0700

    modify carousel

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.slider');
+//   var instances = M.Carousel.init(elems);
+// });
+
 document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instances = M.Carousel.init(elems);
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems);
 });
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems);
+});
