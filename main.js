05031cb878092d311ffcffe833b667c036270b06
Kevin Parine <kparine314@gmail.com>
2018-11-06 13:52:18 -0800

    Edit Theme

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.slider');
-  var instances = M.Slider.init(elems);
+  var instances = M.Slider.init(elems, {indicators: false, interval: 4500, duration: 600});
+
 });
 
